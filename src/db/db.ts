2fe252b6b128bf63dcbea07abac58716d11df4b6
mlieshoff <michael.lieshoff@gmail.com>
2025-04-05 18:30:09 +0200

    Add pool and logs

--- src/db/db.ts
+++ src/db/db.ts
@@ -6,6 +6,9 @@ const sql = postgres({
     user: Config.services.db.db_user,
     pass: Config.services.db.db_password,
     db: Config.services.db.db_name,
+    max: 20,
+    idle_timeout: 20,
+    max_lifetime: 60 * 30,
     debug: true
 })
 
--- src/useCases/keyvalues.ts
+++ src/useCases/keyvalues.ts
@@ -9,6 +9,7 @@ export class KeyValues implements IKeyValues {
     constructor(private readonly service: IKeyValuesService) {}
 
     async getAll(limit: number, offset: number) {
+        console.log('Incoming request at /');
         return await this.service.getAll(limit, offset);
     }
 
