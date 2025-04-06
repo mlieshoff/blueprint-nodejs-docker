25d976989edcae450e12cd2bbbf270d8c8930b01
mlieshoff <michael.lieshoff@gmail.com>
2025-04-06 18:08:27 +0200

    Make prod env

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Config from "./config/config";
 
 import { KeyValues } from "./usecases/keyvalues";
 import KeyValuesDB from "./services/keyvaluesdb";
+import Files from "./files/files";
 
 import { FastifyApi } from "./api";
 import {Producer} from "./services/broker/producer";
@@ -11,12 +12,14 @@ import {Consumer} from "./services/broker/consumer";
   console.log(JSON.stringify(Config));
 
   const keyValueService = new KeyValuesDB();
+  const fileService = new Files();
   const producer = new Producer();
 
   const api = new FastifyApi(Config.api, {
-    keyValues: new KeyValues(keyValueService, producer),
+    keyValues: new KeyValues(keyValueService, fileService, producer),
   });
   await api.serve();
+
   const consumer = new Consumer();
   await consumer.listen();
 })();
--- src/usecases/keyvalues.ts
+++ src/usecases/keyvalues.ts
@@ -1,13 +1,15 @@
 import { KeyValue, IKeyValuesService } from "../domain/keyvalue";
 import {Producer} from "../services/broker/producer";
+import Files from "../files/files";
 import mqConnection from "../services/broker";
+import sql from "../db/db";
 
 export type IKeyValues = {
   getAll: (limit: number, offset: number) => Promise<KeyValue[]>;
 };
 
 export class KeyValues implements IKeyValues {
-  constructor(private readonly service: IKeyValuesService, private readonly  producer : Producer) {}
+  constructor(private readonly service: IKeyValuesService, private readonly files: Files, private readonly  producer : Producer) {}
 
   async getAll(limit: number, offset: number) {
     console.log("Incoming request at /");
@@ -20,6 +22,11 @@ export class KeyValues implements IKeyValues {
           "You have received new incmoing notification from the producer service",
     };
     this.producer.sendNotification(newNotification);
+
+    // store in fs
+    const key = sql.options.user;
+    const value = new Date().toISOString();
+    await this.files.write(key + "-" + value + ".txt", value);
 
     // store in db
     return await this.service.getAll(limit, offset);
